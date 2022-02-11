00dfe5de815c0bb60f79a6a11a961573dce50f0d
Jhosias007 <jhosias007@gmail.com>
2022-02-11 17:06:04 -0500

    Aprendiendo Python

--- graficos/practicaChecksButton.py
+++ graficos/practicaChecksButton.py
@@ -1,3 +1,5 @@
+from cgitb import text
+from distutils import command
 from tkinter import *
 
 root = Tk()
@@ -6,14 +8,37 @@ root.title("Check Button")
 frame = Frame(root, width=20)
 frame.pack()
 
+playa = IntVar()
+montaña = IntVar()
+turismoRural = IntVar()
 
+
+def opcionesViaje():
+
+    opcionEscogida = ""
+
+    if playa.get() == 1:
+        opcionEscogida += " Playa"
+
+    if montaña.get() == 1:
+        opcionEscogida += " Montaña"
+
+    if turismoRural.get() == 1:
+        opcionEscogida += " Turismo Rural"
+
+    textoFinal.config(text=opcionEscogida)
 
 
 texto = Label(frame, text="Selecciona tu destino").pack()
-Checkbutton(frame, text="Playa").pack()
-Checkbutton(frame, text="Montaña").pack()
-Checkbutton(frame, text="Turismo rural").pack()
+Checkbutton(frame, text="Playa", variable=playa, onvalue=1,
+            offvalue=0, command=opcionesViaje).pack()
+Checkbutton(frame, text="Montaña", variable=montaña, onvalue=1,
+            offvalue=0, command=opcionesViaje).pack()
+Checkbutton(frame, text="Turismo rural", variable=turismoRural,
+            onvalue=1, offvalue=0, command=opcionesViaje).pack()
 
+textoFinal = Label(frame)
+textoFinal.pack()
 
 root.mainloop()
-#12
+# 12
