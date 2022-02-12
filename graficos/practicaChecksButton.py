8a1d55045f99267c2281b8724f8f36e5f45cf870
Jhosias007 <jhosias007@gmail.com>
2022-02-12 16:02:41 -0500

    Aprendiendo Python

--- graficos/practicaChecksButton.py
+++ graficos/practicaChecksButton.py
@@ -30,8 +30,10 @@ def opcionesViaje():
 texto = Label(frame, text="Selecciona tu destino").pack()
 Checkbutton(frame, text="Playa", variable=playa, onvalue=1,
             offvalue=0, command=opcionesViaje).pack()
+
 Checkbutton(frame, text="Montaña", variable=montaña, onvalue=1,
             offvalue=0, command=opcionesViaje).pack()
+
 Checkbutton(frame, text="Turismo rural", variable=turismoRural,
             onvalue=1, offvalue=0, command=opcionesViaje).pack()
 
@@ -40,4 +42,43 @@ textoFinal.pack()
 
 
 root.mainloop()
-# 12
+# 20
+
+
+"""
+Checkbutton:
+    Similar al Radiobuttom, pero los Checkbutton se utilizan para hacer una seleccion multiple
+    Sintaxis:
+        ejemploCheckbutton = Checkbutton(root, text="Buenos Dias", variable=var2,
+                                            onvalue=True, offvalue, command=ejecutar, *)
+
+    Podemos obtener su valor relacionando el Checkbutton a una variable y con otros parametros como
+    onvalue=True, offValue=False; (podemos poner cualquier tipo de variable, solo es un ejemplo).
+    De este modo y asociando el Checkbutton a una funcion podremos obtener cuales de las casillas estan
+    seleccionadas.
+    
+    Lo que haiga en value se almacena en la variable pasada por parametros
+
+Parametros:
+    value:
+        Se utiliza para asignar un valor cuando el objeto este seleccionado, por ejemplo, podemos poner esto:
+            value=1; value="a";
+        Podemos obtener su valor asignandole una variable y un valor. Usariamos estos parametros:
+            variable=varX, value=1
+        De este modo podriamos concatenarlo a una funcion que nos indique si el valor que hay es 1 o otro(dependiendo de la cantidad de Radiobuttons que haiga)
+
+    variable:
+        Se utiliza para almacenar un valor cuando el objeto este seleccionado. Ej:
+            variable=var1
+
+    onvalue:
+        Con este parametro podemos retornar un valor a la variable asociada cuando la casilla se encuentre
+        activada. Ej:
+            onvalue=True; onvalue=1
+    
+    offvalue:
+        Con este parametro podemos retornar un valor a la variable asociada cuando la casilla se encuentre
+        inactiva. Ej:
+            offvalue=False; offvalue=2
+
+"""
--- graficos/practicaRadioButton.py
+++ graficos/practicaRadioButton.py
@@ -1,4 +1,3 @@
-from msilib.schema import RadioButton
 from tkinter import *
 
 root = Tk()
@@ -25,3 +24,32 @@ etiqueta = Label(root)
 etiqueta.pack()
 
 root.mainloop()
+
+
+"""
+
+Radiobutton:
+    Los Radiobutton se usan para marcar una opcion entre varias, podemos obtener el valor seleccionado 
+    con el parametro "value" y podemos asignarle un comando con "command"
+    
+    Sintaxis:
+        ejemploRadioButton = Radiobutton(root, text="Hola", variable=var1, value=1, *)
+    
+    Cuando la casilla es seleccionada, se almacena lo que haiga en el parametro value dentro de la
+    variable indicada en los parametros. De este modo y con una funcion en cada Radiobutton podremos
+    hallar que casilla esta seleccionada
+
+    Lo que hay en value se almacena en la variable asociada
+
+Parametros:
+    value:
+        Se utiliza para asignar un valor cuando el objeto este seleccionado, por ejemplo, podemos poner esto:
+            value=1; value="a";
+        Podemos obtener su valor asignandole una variable y un valor. Usariamos estos parametros:
+            variable=varX, value=1
+        De este modo podriamos concatenarlo a una funcion que nos indique si el valor que hay es 1 o otro(dependiendo de la cantidad de Radiobuttons que haiga)
+
+    variable:
+        Se utiliza para almacenar un valor cuando el objeto este seleccionado. Ej:
+            variable=var1
+"""
