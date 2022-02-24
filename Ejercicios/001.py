7b646c0293c675c0cec5932407421ed3c05a63b6
Jhosias007 <jhosias007@gmail.com>
2022-02-23 19:26:55 -0500

    Aprendiendo Python

--- Ejercicios/001.py
+++ Ejercicios/001.py
@@ -10,8 +10,59 @@ class App():
 
         # ? Funciones llamadas
         self.generarMenu()
-        self.generarPlantilla(3)
+        self.generarPlantilla(1)
         self.root.mainloop()
+
+        # ? Conexion a SQLite3
+        self.conexion = sqlite3.connect("Base.db")
+        self.cursor = self.conexion.cursor()
+
+    def crearTabla(self):
+        self.cursor.execute(
+            "CREATE TABLE ALUMNOS (NOMBRE VARCHAR (50), NOTA INTEGER)")
+
+    def añadirAlumnoFuncion(self):
+        self.rootAñadirAlumno = Tk()
+
+        self.labelTituloAñadirAlumno = Label(
+            self.rootAñadirAlumno, text="Añadir Alumno")
+        self.labelTituloAñadirAlumno.grid(row=0, column=0, columnspan=2)
+
+        self.labelNombreAñadirAlumno = Label(
+            self.rootAñadirAlumno, text="Nombre: ")
+        self.labelNombreAñadirAlumno.grid(row=1, column=0, padx=10, pady=10)
+
+        self.labelNotaAñadirAlumno = Label(
+            self.rootAñadirAlumno, text="Nota: ")
+        self.labelNotaAñadirAlumno.grid(row=2, column=0, padx=10, pady=10)
+
+        self.stringVarNombreAñadirAlumno = StringVar()
+        self.entryNombreAñadirAlumno = Entry(self.rootAñadirAlumno)
+        self.entryNombreAñadirAlumno.grid(row=1, column=1, padx=10, pady=10)
+
+        self.stringVarNotaAñadirAlumno = StringVar()
+        self.entryNotaAñadirAlumno = Entry(self.rootAñadirAlumno)
+        self.entryNotaAñadirAlumno.grid(row=2, column=1, padx=10, pady=10)
+
+        self.buttonEnviarAñadirAlumno = Button(
+            self.rootAñadirAlumno, text="Guardar Alumno", command=lambda: self.capturarDatosAñadirAlumno())
+        self.buttonEnviarAñadirAlumno.grid(
+            row=3, column=0, columnspan=2, padx=10, pady=10)
+
+        self.rootAñadirAlumno.mainloop()
+        self.cursor.execute(
+            "INSERT INTO ALUMNOS VALUES (?, ?)", self.infoAlumnos)
+
+    def capturarDatosAñadirAlumno(self):
+        self.infoAlumnoAñadir = (
+            self.stringVarNombreAñadirAlumno.get(), self.stringVarNotaAñadirAlumno.get())
+
+        self.cursor.execute("INSERT INTO ALUMNOS VALUES (?, ?)", self.infoAlumnoAñadir)
+
+        self.rootAñadirAlumno.destroy()
+
+    def eliminarAlumnoFuncion(self):
+        pass
 
     def generarMenu(self):
         self.menu = Menu(self.root)
@@ -38,6 +89,7 @@ class App():
         self.menu.add_cascade(menu=self.ayuda, label="Ayuda")
 
     def generarPlantilla(self, cantAlumnos: int):
+
         # * Labels
         self.cantAlumnos = cantAlumnos
         self.labelTitulo = Label(self.root, text="ALUMNOS")
@@ -62,18 +114,12 @@ class App():
         # * Buttons
 
         self.añadirAlumno = Button(
-            self.root, text="Añadir Alumno", command=lambda: self.funcionAñadirAlumno())
+            self.root, text="Añadir Alumno", command=lambda: self.añadirAlumnoFuncion())
         self.añadirAlumno.grid(row=self.varFila, column=0, padx=10, pady=10)
 
         self.eliminarAlumno = Button(
-            self.root, text="Eliminar Alumno", command=lambda: self.funcionEliminarAlumno())
+            self.root, text="Eliminar Alumno", command=lambda: self.eliminarAlumnoFuncion())
         self.eliminarAlumno.grid(row=self.varFila, column=2, padx=10, pady=10)
-
-    def funcionAñadirAlumno(self):
-        self.cantAlumnos += 1
-
-    def funcionEliminarAlumno(self):
-        pass
 
 
 miApp = App()
